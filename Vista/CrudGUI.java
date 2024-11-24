b143e5cdde2a5aa35dddf9d702b67f69f63bca94
yurimdz <YURIDIA.MENDOZA@ucr.ac.cr>
2024-11-24 17:28:24 -0600

    a

--- Vista/CrudGUI.java
+++ Vista/CrudGUI.java
@@ -6,6 +6,22 @@ import java.io.File;
 
 public class CrudGUI extends JFrame {
 
+    public static void main(String[] args) {
+       
+        CrudGUI interfaz = new CrudGUI();
+       
+        //Configuracion de la ventana principal
+        interfaz.setTitle("Menú Principal");
+        interfaz.setSize(800, 600);
+        interfaz.setBackground(Color.WHITE);
+        interfaz.setResizable(false);
+        interfaz.setVisible(true);
+        interfaz.setLocationRelativeTo(null);
+        interfaz.setDefaultCloseOperation(EXIT_ON_CLOSE); 
+         
+    }
+
+
     public CrudGUI() {
 
         //Configuracion de la ventana principal
